43a22eb1533ac6428dc8633d796bdd7b59d783a0
RahulKakade24 <kakaderahul86@gmail.com>
2026-08-20 12:19:51 +0530

    Clean workspace for a fresh start

--- src/components/index.ts
+++
@@ -1,8 +0,0 @@
-import '../style.css'
-export * from './Button/Button'
-export * from './Card/Card'
-export * from './Modal/Modal'
-export * from './Input/Input'
-export * from './tooltip/Tooltip'
-export * from './carousel/Carousel'
-export * from './layout/Layout'
